fae13c6ed014593b43e5882d3e9afe8129911015
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-04-28 17:37:33 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ end
 
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times do 
+  number_of_times.times do
     puts phrase
  end
 end
