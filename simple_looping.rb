ba9463264e4b6adaed4d9da578f9c77d49418651
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-04-28 17:38:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
     i += 1
-    if i <= number_of_times
+    if i < number_of_times
       break
     end
   end
